efdcdc5d3215131a2f7d0cdd5c00ff1004d69706
Quentin <qr3naud@gmail.com>
2025-05-02 21:24:35 -0400

    Update scraper_api.py

--- scraper_api.py
+++ scraper_api.py
@@ -18,4 +18,4 @@ def run_scraper():
     return jsonify({"status": "ok", "message": "Scraper started"}), 200
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(host="0.0.0.0", port=10000)
